e5fde1bed2f28cb3ea4bdb4a97d1d04960ba827d
Alex C Thain <45198025+alexcharlesthain@users.noreply.github.com>
2019-01-08 15:13:25 +0000

    Update Jenkins Install Script.sh

--- Jenkins Install Script.sh
+++ Jenkins Install Script.sh
@@ -1,11 +1,11 @@
 echo ssh enter ip address of Azure VM
 
 sudo apt-get update
-sudo apt install openjdk-8-jre-headless
+sudo apt install openjdk-8-jre-headless -y
 wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 echo "deb https://pkg.jenkins.io/debian binary/" | sudo tee -a /etc/apt/sources.list
 sudo apt-get update
-sudo apt-get install jenkins
+sudo apt-get install jenkins -y
 
 sudo systemctl start jenkins
 sudo systemctl status jenkins
